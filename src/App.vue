b097187df88fd5a600a4cc0947561cbf0bac8315
Lee Hyungi(Mike) <hg.lee@dtonic.io>
2023-02-10 17:12:07 +0900

    feat: vuex 연습

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view />
+    <router-view /> -->
+    <h4>
+      Mutations에 등록된 변이 핸들러 method를 commit method를 통해 호출한다.
+    </h4>
+    <button @click="$store.commit('increment', { amount: 3 })">증가</button>
+    <button @click="$store.commit('decrement', { amount: 3 })">감소</button>
+    {{ count }}
+    <p>a: {{ countObj.a }}</p>
+    <p>b: {{ countObj.b }}</p>
+    <button @click="increment('a')">a 숫자 증가</button>
+    <button @click="increment('b')">b 숫자 증가</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      countObj: {
+        a: 0,
+      },
+    };
+  },
+  mounted() {
+    // this.countObj["b"] = 0;
+    // 객체에 동적으로 반응성 속성을 추가, 컴포넌트가 제대로 업데이트
+    this.$set(this.countObj, "b", 0);
+  },
+  methods: {
+    increment(type) {
+      this.countObj[type]++;
+    },
+  },
+  computed: {
+    count() {
+      // count 상태값이 변경되면, 자동으로 계산된 속성이 변경되고, 관련 DOM이 업데이트
+      return this.$store.state.count;
+    },
+  },
+};
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  // 변이 핸들러 method
+  mutations: {
+    increment(state, payload) {
+      state.count += payload.amount;
+    },
+    decrement(state, payload) {
+      state.count -= payload.amount;
+    },
+  },
+});
 
 Vue.config.productionTip = false;
 
